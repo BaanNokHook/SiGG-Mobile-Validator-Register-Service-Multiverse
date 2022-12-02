981f84cb4e90f9b204ce33e25228d3e2a4b68066
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-02 17:11:53 +0700

    SiGG-Mobile-Validator-Register-Service-Multiverse-Quarter-IV

---
+++ MongoDB/Models/DbConfiguration.cs
@@ -0,0 +1,10 @@
+namespace MongoDB_CRUD.Models
+{
+      public class DbConfiguration  
+      {
+            public string CollectionName { get; set; }  
+            public string ConnectionString { get; set; }
+            public string DatabaseName { get; set; }  
+      }
+}
+
